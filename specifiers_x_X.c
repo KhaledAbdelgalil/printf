c126034f97bde330d117e3eeeb6f9b93540fb47e
KhaledAbdelgalil <khaled.abdelgalil96@gmail.ccom>
2023-11-14 19:24:15 -0800

    handling more precision3

--- specifiers_x_X.c
+++ specifiers_x_X.c
@@ -1,6 +1,6 @@
 #include "common_utils.h"
 #include <stdarg.h>
-
+#include <limits.h>
 
 /**
  * handle_zero - handleZeroCase in hexa
@@ -9,12 +9,18 @@
  */
 u_int handle_zero(GLOBALBUFFER *printBuffer)
 {
-	u_int usedWidth = 1, lenPadding = 0;
+	u_int lenDigits = 1, usedWidth = 1, lenPadding = 0;
+	u_int padding = 0, i = 0;
 
-	while (usedWidth++ < printBuffer->params.width)
+	if (printBuffer->params.percision != UINT_MAX
+			&& printBuffer->params.percision > lenDigits)
+		padding = printBuffer->params.percision - lenDigits;
+	while (padding + usedWidth++ < printBuffer->params.width)
 		add_to_buffer(' ', printBuffer), lenPadding++;
 	add_to_buffer('0', printBuffer);
-	return (1 + lenPadding);
+	while (i < padding)
+		add_to_buffer('0', printBuffer), i++;
+	return (1 + lenPadding + padding);
 }
 /**
  * handle_hexaSmall - Adds integer bytes as hexaSmall
@@ -28,9 +34,9 @@ u_int handle_hexaSmall(va_list args, GLOBALBUFFER *printBuffer)
 {
 	ul_int num;
 	u_char bits[128], ones = 0;
-	int idx = 0;
-	u_int len = 0, usedWidth = 0, lenPadding = 0;
-	int hexDigit = 0, rem = 0;
+	u_int len = 0, usedWidth = 0, lenPadding = 0, lenDigits = 0;
+	u_int padding = 0, i = 0;
+	int idx = 0, hexDigit = 0, rem = 0;
 
 	if (printBuffer->params.l_modifier)
 		num = (ul_int)va_arg(args, ul_int);
@@ -48,24 +54,22 @@ u_int handle_hexaSmall(va_list args, GLOBALBUFFER *printBuffer)
 			hexDigit = rem + 48;
 		else
 			hexDigit = rem - 10 + 'a';
-		bits[len++] = hexDigit;
-		num = num / 16;
+		bits[len++] = hexDigit, num = num / 16;
 	}
-	idx = (int)len - 1;
-	usedWidth = len + 2 * (printBuffer->params.hashtag_flag);
-	while (usedWidth++ < printBuffer->params.width)
+	lenDigits = len;
+	idx = (int)len - 1, usedWidth = len + 2 * (printBuffer->params.hashtag_flag);
+	if (printBuffer->params.percision != UINT_MAX
+			&& printBuffer->params.percision > lenDigits)
+		padding = printBuffer->params.percision - lenDigits;
+	while (padding + usedWidth++ < printBuffer->params.width)
 		add_to_buffer(' ', printBuffer), lenPadding++;
 	if (printBuffer->params.hashtag_flag)
-	{
-		add_to_buffer('0', printBuffer);
-		add_to_buffer('x', printBuffer);
-		ones = 2;
-	}
+		add_to_buffer('0', printBuffer), add_to_buffer('x', printBuffer), ones = 2;
+	while (i < padding)
+		add_to_buffer('0', printBuffer), i++;
 	while (idx >= 0)
-	{
 		add_to_buffer(bits[idx--], printBuffer);
-	}
-	return (lenPadding + len + ones);
+	return (padding + lenPadding + len + ones);
 }
 
 /**
@@ -80,9 +84,9 @@ u_int handle_hexaCapital(va_list args, GLOBALBUFFER *printBuffer)
 {
 	ul_int num;
 	u_char bits[128], ones = 0;
-	int idx = 0;
-	u_int len = 0, lenPadding = 0, usedWidth = 0;
-	int hexDigit = 0, rem = 0;
+	u_int len = 0, lenPadding = 0, usedWidth = 0, lenDigits = 0;
+	u_int padding = 0, i = 0;
+	int hexDigit = 0, rem = 0, idx = 0;
 
 	if (printBuffer->params.l_modifier)
 		num = (ul_int)va_arg(args, ul_int);
@@ -103,19 +107,19 @@ u_int handle_hexaCapital(va_list args, GLOBALBUFFER *printBuffer)
 		bits[len++] = hexDigit;
 		num = num / 16;
 	}
+	lenDigits = len;
 	usedWidth = len + 2 * (printBuffer->params.hashtag_flag);
-	while (usedWidth++ < printBuffer->params.width)
+	if (printBuffer->params.percision != UINT_MAX
+			&& printBuffer->params.percision > lenDigits)
+		padding = printBuffer->params.percision - lenDigits;
+	while (padding + usedWidth++ < printBuffer->params.width)
 		add_to_buffer(' ', printBuffer), lenPadding++;
 	if (printBuffer->params.hashtag_flag)
-	{
-		add_to_buffer('0', printBuffer);
-		add_to_buffer('X', printBuffer);
-		ones = 2;
-	}
+		add_to_buffer('0', printBuffer), add_to_buffer('X', printBuffer), ones = 2;
 	idx = (int)len - 1;
+	while (i < padding)
+		add_to_buffer('0', printBuffer), i++;
 	while (idx >= 0)
-	{
 		add_to_buffer(bits[idx--], printBuffer);
-	}
-	return (lenPadding + len + ones);
+	return (padding + lenPadding + len + ones);
 }
